bcc80cd306dcb456630ab6ce2bf50b516f4baa58
dysan66 <dysan66@op.pl>
2023-04-30 22:48:33 +0200

    zadanie domowe dzien 6

---
+++ ChallengeApp2/ChallengeApp2/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace ChallengeApp2
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+        public Employee(string firstName, string secondName, string age)
+        {
+            this.FirstName = firstName;
+            this.SecondName = secondName;
+            this.Age = age;
+        }
+        public string FirstName { get; private set; }
+        public string SecondName { get; private set; }
+        public string Age { get; private set; }
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int score)
+        {
+            this.score.Add(score);
+        }
+    }
+}
--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChallengeApp2;
+
+Employee employee1 = new Employee("Adam", "Kowalski", "21");
+Employee employee2 = new Employee("Monika", "Nowak", "30");
+Employee employee3 = new Employee("Dawid", "Kukiz", "25");
+
+employee1.AddScore(5);
+employee1.AddScore(2);
+employee1.AddScore(10);
+employee1.AddScore(1);
+employee1.AddScore(3);
+
+employee2.AddScore(3);
+employee2.AddScore(7);
+employee2.AddScore(6);
+employee2.AddScore(1);
+employee2.AddScore(10);
+
+employee3.AddScore(2);
+employee3.AddScore(10);
+employee3.AddScore(9);
+employee3.AddScore(7);
+employee3.AddScore(2);
+
+List<Employee> employees = new List<Employee>()
+{
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach ( var employee in employees )
+{
+    if (employee.Result > maxResult)
+    {
+        maxResult = employee.Result;
+        employeeWithMaxResult = employee;
+    }
+}
+
+Console.WriteLine("Najlepszym pracownikiem jest: " + employeeWithMaxResult.FirstName + " " + employeeWithMaxResult.SecondName + " " + "lat" + " " + employeeWithMaxResult.Age);
+Console.WriteLine("z wynikiem " +  maxResult);
